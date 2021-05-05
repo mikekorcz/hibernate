be991ac253524f1197d23b7a7457dedf8f069799
Michal <mkorcz01@gmail.com>
2021-05-05 10:15:59 +0200

    added SaveEntityApp

---
+++ src/main/java/pl/mkorcz/SaveEntityApp.java
@@ -0,0 +1,29 @@
+package pl.mkorcz;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import pl.mkorcz.entity.Employee;
+
+public class SaveEntityApp {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        configuration.addAnnotatedClass(Employee.class);
+
+        SessionFactory factory = configuration.buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        Employee employee = new Employee();
+        employee.setIdEmployee(1);
+        employee.setFirstName("Mike");
+        employee.setLastName("Kowalski");
+        employee.setSalary(25000);
+
+        session.beginTransaction();
+        session.save(employee);
+        session.getTransaction().commit();
+        factory.close();
+    }
+}
