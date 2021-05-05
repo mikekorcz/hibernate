2de6b521107a334f3a989f2a02adb6d192ceaa70
Michal <mkorcz01@gmail.com>
2021-05-05 12:01:29 +0200

    added UpdateEntity

--- src/main/java/pl/mkorcz/UpdateEntity.java
+++ src/main/java/pl/mkorcz/UpdateEntity.java
@@ -1,4 +1,34 @@
 package pl.mkorcz;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import pl.mkorcz.entity.Employee;
+
 public class UpdateEntity {
+    public static void main(String[] args) {
+
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        configuration.addAnnotatedClass(Employee.class);
+
+        SessionFactory factory = configuration.buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        session.beginTransaction();
+
+        Employee employee= session.get(Employee.class, 10);
+        employee.setFirstName("Adam");
+        employee.setLastName("Mickiewicz");
+
+        session.getTransaction().commit();
+
+
+        System.out.println("Updated employee: " +employee);
+
+
+        factory.close();
+    }
+
+
 }
